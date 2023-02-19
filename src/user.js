5184a3a158f908827b655fa21cec5d4cb0239272
durgabhemesh <114475114+durgabhemesh@users.noreply.github.com>
2023-02-19 08:42:06 +0530

    Update user.js

--- src/user.js
+++ src/user.js
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const User = (props) => {
-  return (
-    <div>
-      <h2>Number</h2>
-        {props.number}
+  const[num,setNum]=useState()
+  const[count,setCount]=useState(0)
+  const[data,setData]=useState([])
+  const df=async()=>{
 
+    const res=await fetch('https://juices-74cd7-default-rtdb.firebaseio.com/Juices/j3.json')
+    const resData=await res.json()
+  
+    console.log(resData.name)
+    setNum(resData.name)
+    setCount(count+1)
+    // setData((pdata)=>{
+    //       return[num,'-',...pdata]
+    //     })
+  }
+  setInterval(()=>{
+    df()
+  },1000)
+  return (
+<>
+    <div className='Container'>
+     
+     
+     {count===6 ? <h2 className= 'gameover'>Game Over</h2>  : <h2 className= 'letsplay'>Let's Play</h2> } 
+     {/* <h1 className='threedots'>{loading ? <ThreeDots 
+height="80" 
+width="80" 
+radius="9"
+color="#4fa94d" 
+ariaLabel="three-dots-loading"
+wrapperStyle={{}}
+wrapperClassName=""
+visible={true}
+ /> : <h1 className='NumberH1'>{num}</h1> }</h1> */}
+ <h1 className='NumberH1'>{num}</h1> 
+ 
+
+{data &&
+<h3>Last Numbers
+  <br />{data }</h3>
+}
     </div>
+   
+     
+    </>
+   
+    
   )
 }
 
